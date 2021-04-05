8225f940dc6a811072cbcb57e0f40625f8e038fb
Angi Benton <angi.benton@jhu.edu>
2021-04-05 19:28:26 -0400

    work on monday - trying to connect spotify + billb

---
+++ phase_C/preprocessing_admins.py
@@ -0,0 +1,25 @@
+import pandas as pd
+#years constrained by the billboard data 
+START_YEAR = '1953'
+END_YEAR = '2017'
+
+#empty dataframe analogous to the Administration relation 
+admins = pd.DataFrame()
+
+#GET PPREZS FROM CSV
+prezs = pd.read_csv("sourceData/presidents.csv") 
+print(prezs) 
+
+#PUT ONLY NECESSARY ATTRIBUTES IN ADMINS
+admins['president'] = prezs['president']
+#extract only the year from the start and end dates 
+admins['startYear'] = prezs.start.str.extract(r'\b(\w+)$', expand=True) 
+admins['endYear'] = prezs.end.str.extract(r'\b(\w+)$', expand=True) 
+
+#constrain to only years that match billboard data 
+admins = admins[(admins.startYear >= START_YEAR) & (admins.startYear <= END_YEAR)]
+
+print(admins)
+
+#export to a text file with no row numbers, no headers, and 4 decimal places 
+admins.to_csv("Administration.txt", index = False, header = False) 
--- phase_C/preprocessing_music.py
+++ phase_C/preprocessing_music.py
@@ -4,15 +4,16 @@
 def removeFeature(artistName):
   return artistName.split(' featuring')[0]
 
+    
 import pandas as pd 
 bb = pd.read_csv("sourceData/billboard_yearly.csv")
-#hotSpotify = pd.read_csv("sourceData/hotSpotify.csv") why is this broken 
+hotSpotify = pd.read_csv("sourceData/hotSpotify.csv", encoding ='latin1') 
 
 #Set up the dataframes for each of our relations 
-#the columns here are just the ones I have needed so far, add more as we go 
-Song = pd.DataFrame(columns = ['songId', 'title', 'artist'], dtype = int)
+Song = pd.DataFrame(columns = ['songId', 'title', 'artist', 'spotifyMatchFound', 'genres', 'key', 'tempo', 
+'danceability', 'energy', 'acousticness', 'instrumentalness', 'liveness', 'valence'], dtype = int)
 BillboardChart = pd.DataFrame(columns = ['songId', 'chartYear', 'chartPosition'], dtype = int)
-Genre = pd.DataFrame()
+Genre = pd.read_csv("sourceData/spotify_genre_list.txt") #might modify this a little 
 SongGenre = pd.DataFrame()
 
 
@@ -20,10 +21,8 @@ SongGenre = pd.DataFrame()
 bb = bb.sort_values(by = ['title', 'artist', 'year']) #sort by song (useful for removing duplicates)
 bb = bb.reset_index(drop=True) 
 bb = bb.drop("Unnamed: 0",axis=1)
-print(bb)
-print(bb.loc[0]['title'])
 
-#extract everything relevant from bb
+#extract everything relevant from bb (Part of Song relation, all of BillboardChart relation)
 curSongId = -1
 curTitle = ""
 curArtist = ""
@@ -38,17 +37,43 @@ for i, row in bb.iterrows():
   new_chart = {'songId': curSongId, 'chartYear': row['year'], 'chartPosition': row['number']}
   BillboardChart = BillboardChart.append(new_chart, ignore_index = True)
 
-#TODO -> FIX HOTSPOTIFY READ CSV  
 
-#TODO -> ASSOCIATE SONG W/ DATA FROM HOTSPOTIFY 
+#criteria for matching a song in the billboard data to one in spotify data:
+#exact matching titles, and artist in billboard is substring of Performer in spotify 
+#change this maybe?
 
-#TODO -> GENRES
+Song = Song.sample(30) #TEMPORARILY make Song much smaller 
+
+Song['spotifyMatchFound'] = 0
+for bbIndex, bbRow in Song.iterrows():
+  print(bbRow['title'])
+  name_matches = hotSpotify[hotSpotify['Song'] == bbRow['title']]
+  for spotIndex, spotRow in name_matches.iterrows():
+    if (bbRow['artist'] in spotRow['Performer']):
+      print("match found!")
+      Song.loc[bbIndex, 'spotifyMatchFound'] = 1
+      Song.loc[bbIndex, 'genres'] = spotRow['spotify_genre']
+      Song.loc[bbIndex, 'key'] = spotRow['key']
+      Song.loc[bbIndex, 'tempo'] = spotRow['tempo']
+      #special spotify characteristics 
+      Song.loc[bbIndex, 'danceability'] = spotRow['danceability']
+      Song.loc[bbIndex, 'energy'] = spotRow['energy']
+      Song.loc[bbIndex, 'acousticness'] = spotRow['acousticness']
+      Song.loc[bbIndex, 'instrumentalness'] = spotRow['instrumentalness']
+      Song.loc[bbIndex, 'liveness'] = spotRow['liveness']
+      Song.loc[bbIndex, 'valence'] = spotRow['valence']
+      break
+  
+
+#TODO -> GENRES -> maybe delete genres that dont have any songs in our dataset?
 
 print(Song)
 BillboardChart = BillboardChart.sort_values(by = ["chartYear"])
-print(BillboardChart)
   
-
+BillboardChart.to_csv("BillboardChart.txt", header = False, index = False)
+Song.to_csv("Song.txt", header = False, index = False)
+Genre.to_csv("Genre.txt", header = False, index = False)
+SongGenre.to_csv("SongGenre.txt", header = False, index = False)
 
 
 
